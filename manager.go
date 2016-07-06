1e8685297064d41d87c737f9f596dbc115903a67
Adrian Witas <awitas@specificmedia.com>
2016-07-06 13:20:37 -0700

    removed license from go files

--- manager.go
+++ manager.go
@@ -1,45 +1,5 @@
-/*
- *
- *
- * Copyright 2012-2016 Viant.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- *  use this file except in compliance with the License. You may obtain a copy of
- *  the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- *  License for the specific language governing permissions and limitations under
- *  the License.
- *
- */
-
-//Package asc - Aersopike datastore manager
 package asc
 
-/*
-
-This library comes with the aerospike datastore manager implementing datastore connectivity manager (dsc)
-Is uses native aerospike client to connect to NoSQL Aerospike server, and adds SQL layer on top of it.
-
-Usage:
-
-
-import (
-    _ "github.com/aerospike/aerospike-client-go"
-    _ "github.com/viant/asc"
-)
-
-{
- 	config := dsc.NewConfig("aerospike", "", "host:127.0.0.1,port:3000,namespace:test,generationColumnName:generation,dateLayout:2006-01-02 15:04:05.000")
-	factory := dsc.NewManagerFactory()
-	manager, err := factory.Create(config)
-}
-
-*/
 
 import (
 	"database/sql"
