a4eb2d65373382372cc29bc4c435463c6d87c793
Terry Zhao <tzhao@TZHA-C02K34NUDKQ1.local>
2017-11-28 13:57:20 -0800

    patched float converion

--- manager.go
+++ manager.go
@@ -79,11 +79,10 @@ func convertIfNeeded(source interface{}) interface{} {
 	case reflect.String, reflect.Bool:
 		return toolbox.AsString(source)
 	case reflect.Float32, reflect.Float64:
-		stringValue := toolbox.AsString(source)
-		if strings.Contains(stringValue, ".") {
-			return stringValue
+		if toolbox.AsFloat(toolbox.AsInt(source)) == source {
+			return toolbox.AsInt(source)
 		}
-		return toolbox.AsInt(stringValue)
+		return toolbox.AsString(source)
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
 		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
 		stringValue := toolbox.AsString(source)
