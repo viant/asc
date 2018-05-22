dff0af9cd0b2e0e7e91af21daa265b2efa6d315b
adrianwit <adrianwit@gmail.com>
2018-05-21 22:52:24 -0700

    added default time layout

--- manager_factory.go
+++ manager_factory.go
@@ -2,13 +2,30 @@ package asc
 
 import (
 	"github.com/viant/dsc"
+	"github.com/viant/toolbox"
+)
+
+const (
+	defaultTimeFormat = "yyyy-MM-dd HH:mm:ss z"
+	DateFormatKey       = "dateFormat"
+	DateLayoutKey       = "dateLayout"
 )
 
 type managerFactory struct{}
 
 func (f *managerFactory) Create(config *dsc.Config) (dsc.Manager, error) {
+	if !config.Has(DateFormatKey) {
+		config.Parameters[DateFormatKey] = defaultTimeFormat
+	}
+
+	if ! config.Has(DateLayoutKey) {
+		config.Parameters[DateLayoutKey] = toolbox.DateFormatToLayout(config.Get(DateFormatKey))
+	}
+
 	var connectionProvider = newConnectionProvider(config)
+
 	manager := &manager{}
+
 	var self dsc.Manager = manager
 	super := dsc.NewAbstractManager(config, connectionProvider, self)
 	manager.AbstractManager = super
